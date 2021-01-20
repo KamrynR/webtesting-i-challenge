691f272be445dee92a05c36afe134121f0025f0c
KamrynR <62856359+KamrynR@users.noreply.github.com>
2021-01-19 20:29:01 -0600

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,42 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  const successfulItem = { ...item }
+
+  if (successfulItem.enhancement === 20) {
+    return successfulItem
+  } else {
+    successfulItem.enhancement += 1
+    return successfulItem
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  const failedItem = { ...item }
+
+  if (failedItem.enhancement < 15) {
+    failedItem.durability -= 5
+  }
+
+  if (failedItem.enhancement >= 15) {
+    failedItem.durability -= 10
+  }
+
+  if (failedItem.enhancement > 16) {
+    failedItem.enhancement -= 1
+  }
+
+  return failedItem
 }
 
 function repair(item) {
-  return { ...item };
+  const repairedItem = { ...item }
+
+  if (repairedItem.durability < 100) {
+    repairedItem.durability = 100
+  }
+
+  return repairedItem
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,46 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe('tests the enchancer', () => {
+
+    test('enhancement success', () => {
+        expect(enhancer
+            .success({
+                enhancement: 20
+            }))
+            .toEqual({
+                enhancement: 20
+            })
+        expect(enhancer
+            .success({
+                enhancement: 13
+            }))
+            .toEqual({
+                enhancement: 14
+            })
+    })
+
+    test('fails', () => {
+        expect(enhancer
+            .fail({
+                enhancement: 13,
+                durability: 80
+            }))
+            .toEqual({
+                enhancement: 13,
+                durability: 75
+            })
+        expect(enhancer
+            .fail({
+                enhancement: 18,
+                durability: 80
+            }))
+            .toEqual({
+                enhancement: 17,
+                durability: 70
+            })
+    })
+    
+    test('repairs item', () => {
+        expect(enhancer.repair({durability: 80})).toEqual({durability: 100})
+    })
+})
